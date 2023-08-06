792db2c8011ae1e6765a3c6e2c7610dba7fefabd
Taleh <t4lex999@gmail.com>
2023-08-07 02:43:46 +0400

    route model binding

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 
 use App\Domain\Services\PredictionService;
 use App\Http\Helpers\ApiHelpers;
-use App\Http\Requests\MatchRequest;
+use App\Http\Requests\WeekRequest;
+use App\Models\Leagues;
 use Illuminate\Http\JsonResponse;
 
 class PredictionController extends Controller
@@ -16,11 +17,11 @@ class PredictionController extends Controller
     {
     }
 
-    public function show(MatchRequest $request): JsonResponse
+    public function show(Leagues $league, WeekRequest $request): JsonResponse
     {
         $week = $request->validated()['week'];
 
-        $predictions = $this->predictionService->getPredictions($week);
+        $predictions = $this->predictionService->getPredictions($league->id,$week);
 
         return ApiHelpers::successResponse('OK', ['predictions' => $predictions]);
 
